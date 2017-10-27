3b8d7e5943659c769c7d53c40e6c47d59d54ea80
Julianne Kim <juliannne@gmail.com>
2017-10-27 00:57:34 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -60,26 +60,27 @@ class Game
   def self.start
     puts "Welcome to Tic Tac Toe!"
     puts "What player game would you like to play? (0-2)"
-    player_num = gets.strip
     puts "Which player should go first and have a token of 'X'? (1 or 2)"
+    puts "For example, write as (1, 1)"
+    input = gets.strip.split(",").map(&:to_i)
+    
     num = gets.strip
 
     case input
-    when "O" && num == "1"
-      Game.new(player_1=Players::Computer.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
-    when "O" && num == "2"
-      Game.new(player_1=Players::Computer.new("O"), player_2=Players::Computer.new("X"), board=Board.new)
-    when "1" && num == "2"
-      Game.new(player_1=Players::Human.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
-    when "1" && num == "2"
-      Game.new(player_1=Players::Compuyer.new("O"), player_2=Players::Human.new("X"), board=Board.new)
-    when "2" && num == "1"
-      Game.new(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
-    when "2" && num == "2"
-      Game.new(player_1=Players::Human.new("O"), player_2=Players::Human.new("X"), board=Board.new)
+    when [0, 1]
+      game = Game.new(player_1=Players::Computer.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
+    when [0, 2]
+      game = Game.new(player_1=Players::Computer.new("O"), player_2=Players::Computer.new("X"), board=Board.new)
+    when [1, 1]
+      game = Game.new(player_1=Players::Human.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
+    when [1, 2]
+      game = Game.new(player_1=Players::Compuyer.new("O"), player_2=Players::Human.new("X"), board=Board.new)
+    when [2, 1]
+      game = Game.new(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+    when [2, 2]
+      game = Game.new(player_1=Players::Human.new("O"), player_2=Players::Human.new("X"), board=Board.new)
     end
 
-    board.display
     game.play
 
     puts "Would you like to play again?(Y/N)"
