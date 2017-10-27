d871fda6a4343cf35be20b742040649db3c1c2ab
Julianne Kim <juliannne@gmail.com>
2017-10-27 00:39:43 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,14 +12,14 @@ module Players
     WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7],[2,5,8], [0,4,8], [6,4,2]]
 
     def move(board)
-      if board.turn_count <= 4 
-        pos = rand(1..9).to_s 
-      end 
+      if board.turn_count <= 4
+        pos = rand(1..9).to_s
+      end
       potential_win = WIN_COMBINATIONS.find do |array|
         (board.cells[0] == token && board.cells[1] == token) || (board.cells[1] == token && board.cells[2] == token) ||
-          (board.cells[0] == token && board.cells[2] == token 
+          (board.cells[0] == token && board.cells[2] == token
       end
-      pos = potencial_win.find { |idx| board.cells[idx] == "" }
+      pos = potencial_win.find { |idx| board.cells[idx] == "" } + 1
     end
 
   end
